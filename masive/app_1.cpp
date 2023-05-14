69a82fd8d5b98c8e9722a002e2c96bab5659af5f
Roman <roma2000kuba@gmail.com>
2023-05-14 09:59:32 +0300

    ex_1 done

---
+++ masive/app_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numb;
+	cout << "input number: " ;
+	cin >> numb;
+	for (int i = 0; i <= numb; i++) {
+		cout << i << " ";
+	}
+		
+	return 0;
+}
