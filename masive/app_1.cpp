d328c6c083f63905b6b819ae58f61dae4b91a001
Roman <roma2000kuba@gmail.com>
2023-05-19 17:20:14 +0300

    ex_6 done

---
+++ masive/app_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	int a = 0;
+	for (int i = 100; i <=999; ++i) {
+		int num_1 = i / 100;
+		int num_2 = (i / 10) % 10;
+		int num_3 = i % 10;
+
+		if (num_1 == num_2 || num_2 == num_3 || num_1 == num_3) {
+			a++;
+		}
+	}
+	cout << "The number of integers in the range 100 to 999 that have two identical digits: " << a<< endl;
+	return 0;
+}
