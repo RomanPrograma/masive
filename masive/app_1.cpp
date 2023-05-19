cb0b7f4afd91ea883e99442d3b292f273ef47dfd
Roman <roma2000kuba@gmail.com>
2023-05-19 16:32:17 +0300

    ex_ 3 done

--- masive/app_1.cpp
+++ masive/app_1.cpp
@@ -4,13 +4,30 @@
 using namespace std;
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n, i;
+    float num[1000], sum = 0.0, average;
 
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int a = 1;
-	int b = 1000;
-	int sum = a + b;
-	float avg = sum / 2;
-	cout << avg;
-	return 0;
+    cout << "Введіть кількість чисел: ";
+    cin >> n;
+
+    while (n > 1000 || n <= 0)
+    {
+        cout << "Error! number should in range of (1 to 1000)." << endl;
+        cout << "Enter the number again: ";
+        cin >> n;
+    }
+
+    for (i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Введіть число: ";
+        cin >> num[i];
+        sum += num[i];
+    }
+
+    average = sum / n;
+    cout << "Середнє арифметичне = " << average;
+    return 0;
 }
+
