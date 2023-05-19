4bac5d47abe4a3e3f960932861d9f5526226bad2
Roman <roma2000kuba@gmail.com>
2023-05-19 13:23:43 +0300

    ex 5 20%

---
+++ masive/app_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+int main() {
+
+    int chislo;
+    cout << "Vvedite chislo ot 1 do 999999: ";
+    cin >> chislo;
+    
+
+    return 0;
+}
